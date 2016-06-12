29c29b3e64f6a7fa9669cd272f2aff3d93a4f2ae
Prashanth Rajaram <prashanthrdev@gmail.com>
2016-06-12 12:29:57 -0500

    Adding interfaces, mousedown and mouseup handling

--- src/eventContentScript.js
+++ src/eventContentScript.js
@@ -1,4 +1,75 @@
 console.log('Content script running [eventContentScript.js]');
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+document.body.addEventListener('mousedown', captureMouseEvent, true);
+document.body.addEventListener('mouseup', captureMouseEvent, true);
+
+function log(msg) {
+	console.log(msg);
+}
+
+/*chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
   console.log(response.farewell);
-});
+});*/
+
+sendRuntimeMessage({greeting: "hello"}, log);
+
+var cmdCache = [];//IMouseAction
+var cmdState = [{
+		type: null,
+		which: 1,
+		time: null,
+	},
+	{
+		type: null,
+		which: 2,
+		time: null,
+	},	
+	{
+		type: null,
+		which: 3,
+		time: null,
+	}
+];
+
+
+function sendRuntimeMessage(message, callback) {
+	chrome.runtime.sendMessage(message, callback);
+}
+function captureMouseEvent(event) {
+	console.log('cmdEvent', event);
+	addCmdToCache(event);
+	updateState(event);
+	handleEvent();
+}
+
+function handleEvent() {
+	console.log('cmdState', cmdState);
+}
+
+function addCmdToCache(event) {
+	cmdCache.unshift({
+		type: event.type,
+		which: event.which,
+		time: Date.now()
+	});
+}
+function updateState(event) {
+	let cmdIndex = getCmdStateIndex(event.which);
+	if(cmdIndex !== -1) {
+		cmdState[cmdIndex].type = event.type;
+		cmdState[cmdIndex].time = Date.now();
+	}
+}
+
+function getCmdState(which) {
+	let cmd = cmdState.find((cmd) => {
+		return cmd.which === which
+	});
+	return cmd;
+}
+function getCmdStateIndex(which) {
+	let cmdIndex = cmdState.findIndex((cmd) => {
+		return cmd.which === which
+	});
+	return cmdIndex;
+}
+
